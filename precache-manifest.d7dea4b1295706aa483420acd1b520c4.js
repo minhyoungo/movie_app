6afcb1284f4468a1645ad8a73860750f88ccb09f
minhyoungo <59827905+minhyoungo@users.noreply.github.com>
2020-08-25 02:38:06 +0900

    Updates

---
+++ precache-manifest.d7dea4b1295706aa483420acd1b520c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4064ebcef6e20be9459d7ff9b520d3",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "eff6c0f8774a4c61fd70",
+    "url": "/movie_app/static/css/main.29c4e79f.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff6c0f8774a4c61fd70",
+    "url": "/movie_app/static/js/main.efd3342e.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
